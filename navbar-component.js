703aa9c6e60e87f430c8807491bd4aae79b8f870
Rossy Lopez <rossy181071@hotmail.com>
2024-11-19 16:27:00 -0600

    Initial commit

---
+++ navbar-component.js
@@ -0,0 +1,6 @@
+ import {LitElement, html, css} from "lit";
+
+export class NavbarComponent extends LitElement {
+
+}
+window.customElements.define('navbar-component', NavbarComponent)
---
+++ root-component.js
@@ -0,0 +1,8 @@
+import { LitElement, html, css, nothing } from 'lit'
+
+export class RootComponent extends LitElement {
+
+
+}
+
+window.customElements.define('root-component', RootComponent)
